f322316f76c67bada7a8077f1912cefb24651fa6
LCUSTOS <lucas.custos@ynov.com>
2023-01-09 10:34:34 +0100

    invoices-price-case.sql

---
+++ invoices-price-case.sql
@@ -0,0 +1,8 @@
+Select InvoiceId, Total,
+       CASE
+           WHEN Total<5 THEN 'Price lower than 5$'
+           WHEN Total<10 THEN 'Price lower than 10$'
+           WHEN Total>10 THEN 'Price greater than 10$'
+       END as 'CASE'
+from invoices
+limit 100
